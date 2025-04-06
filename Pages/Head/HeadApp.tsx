6be9b6615c2dc3c7985634f2d07bbc172176db3e
alanr <alanrodrigolaime@gmail.com>
2025-04-06 11:13:25 -0400

    pequeños cambios

--- Pages/Head/HeadApp.tsx
+++ Pages/Head/HeadApp.tsx
@@ -3,15 +3,18 @@ import './HeadStyle.css'
 function Head() {
   return (
     <>
-     <header>
-        <h1>MANGO - shop</h1>
+      <nav>
         <ul>
-            <li><a href="" data-title='gatos'>Inicio</a></li>
-            <li><a href="">Articulos</a></li>
-            <li><a href="">Noticias y Foros</a></li>
-            <li><a href="">Registro</a></li>
-            <li></li>
-        </ul>
+              <li><a href="" data-title='gatos'>Inicio</a></li>
+              <li><a href="">Articulos</a></li>
+              <li><a href="">Noticias y Foros</a></li>
+              <li><a href="">Registro</a></li>
+              <li></li>
+          </ul>
+      </nav>
+     <header>
+        <h1>Titulo de la pagina</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dicta, asperiores earum corporis iure eveniet voluptas itaque animi dolores molestias?</p>
      </header>
     </>
   )
